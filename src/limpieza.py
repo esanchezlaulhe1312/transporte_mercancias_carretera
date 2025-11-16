8e7069683e86743d29dcd07529b3ce198f8bfa18
esanchezlaulhe1312 <esanchezlaulhe@gmail.com>
2025-11-16 14:33:37 +0100

    Análisis y Principales KPI

--- src/limpieza.py
+++ src/limpieza.py
@@ -7,6 +7,8 @@ import seaborn as sns
 import re
 import tex as tx
 
+
+# Función: Limpiar valores numéricos
 def limpiar_valor_numerico(valor):
 
     # Si ya es un número, devolverlo
@@ -38,3 +40,30 @@ def limpiar_valor_numerico(valor):
     
     # Para cualquier otro tipo, devolver NaN
     return np.nan
+
+
+
+# Función: Limpiar el Año
+def limpiar_anio(texto):
+    if pd.isna(texto): return None
+    match = re.search(r'\d{4}', str(texto)) # Busca 4 dígitos seguidos
+    if match: return int(match.group(0))
+    return None
+
+
+
+# Función: Limpiar la Superficie
+def limpiar_superficie(texto):
+    if pd.isna(texto): return 0.0 # Tu petición: NaN -> 0
+    
+    # Quitamos los puntos de miles ("14.569" -> "14569")
+    # Si no hacemos esto, Python pensará que es 14 coma 569
+    texto_limpio = str(texto).replace('.', '')
+    
+    # Convertimos la coma decimal a punto (por si acaso hubiera decimales reales)
+    texto_limpio = texto_limpio.replace(',', '.')
+    
+    try:
+        return float(texto_limpio)
+    except:
+        return 0.0 # Si falla por texto raro, devuelve 0
